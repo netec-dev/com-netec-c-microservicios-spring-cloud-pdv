8f3ce63c2e0446068c9357fb2c18e31dd09576e7
Pedro Cazu <training@pcabrera.com>
2019-05-21 01:46:52 -0500

    Balanceo de carga con descubrimiento

--- servicio-de-pedidos/src/main/java/com/netec/c/microservicios/spring/cloud/pdv/pedido/PedidoApp.java
+++ servicio-de-pedidos/src/main/java/com/netec/c/microservicios/spring/cloud/pdv/pedido/PedidoApp.java
@@ -2,9 +2,8 @@ package com.netec.c.microservicios.spring.cloud.pdv.pedido;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,11 +12,12 @@ import com.netec.c.microservicios.spring.cloud.pdv.pedido.repo.PedidoRepository;
 
 
 @SpringBootApplication
-@RibbonClients({
-	@RibbonClient(name = "servicio-de-cuentas"),
-	@RibbonClient(name = "servicio-de-clientes"),
-	@RibbonClient(name = "servicio-de-productos")
-})
+//@RibbonClients({
+//	@RibbonClient(name = "servicio-de-cuentas"),
+//	@RibbonClient(name = "servicio-de-clientes"),
+//	@RibbonClient(name = "servicio-de-productos")
+//})
+@EnableDiscoveryClient
 public class PedidoApp {
 
 	@LoadBalanced
